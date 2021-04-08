f6d8a322124be3322806564094489e3f263da3a5
vmlgithub <81070841+vmlgithub@users.noreply.github.com>
2021-04-08 15:05:31 +0800

    Add files via upload

---
+++ php-assignment-13-coin-throw-final.php
@@ -0,0 +1,24 @@
+<?php
+$head = 0;
+$tail = 0;
+
+echo "<b><u>Starting the program</u></b><br>";
+    for($x=1;$x<=5000;$x++){
+        
+        echo "Attempt #".$x;      
+        $randomizer = rand(1,2);
+
+        if($randomizer==1){
+            $head++;
+            echo ": Throwing a coin... It's a head! ... Got ".$head." head(s)"." so far and ".$tail."tail(s) so far<br>";
+        }
+
+        else{
+            $tail++;
+            echo ": Throwing a coin... It's a tail! ... Got ".$head." head(s)"." so far and ".$tail."tail(s) so far<br>";
+        }
+        
+    }    
+    echo "<b><u>Ending the program - thank you!</u></b>";
+
+?>
